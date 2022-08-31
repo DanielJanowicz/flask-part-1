28aceedee05a42e78430c875944154d0dbb574f2
DanielJanowicz <102616987+DanielJanowicz@users.noreply.github.com>
2022-08-31 17:15:51 -0400

    Debug 1
    Importing code from HHA 504 to flask to investigate connection error with main.py

---
+++ test.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello Dan!'
+
+@app.route('/careers')
+def careers():
+    return 'THis is the career page!'
+
+@app.route('/dashboard')
+def dashboard():
+    return 'This is the website dashboard!'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=80)
